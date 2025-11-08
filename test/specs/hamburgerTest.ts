60afe82a6757a1dd32561036c3f63e381a996271
ZealanTannerMtech <zealan.tanner0869@stu.mtec.edu>
2025-11-07 21:29:28 -0700

    ordered hamburger tests nicer

--- test/specs/hamburgerTest.ts
+++ test/specs/hamburgerTest.ts
@@ -8,9 +8,9 @@ describe(`Opening`, () => {
     beforeEach("Login", async () => {
         await Login.open();
         await Login.login(validUser)
-        await base.openRandomPage()
     })
     it(`Should open hamburger menu from random page`, async () => {
+        await base.openRandomPage()
         await base.Hamburger.assertOpen()
         await base.Hamburger.assertClose()
     })
@@ -20,25 +20,31 @@ describe(`Hamburger menu`, () => {
     beforeEach("Login", async () => {
         await Login.open();
         await Login.login(validUser)
-        await base.Hamburger.clickOpen()
     })
     describe(`All Items button`, async () => {
         it(`should assert "All Items" button`, async () => {
+            await base.openRandomPage()
+            await base.Hamburger.clickOpen()
             await base.Hamburger.assertAllItems()
         })
     })
     describe(`About button`, async () => {
         it(`should assert "About" button`, async () => {
+            await base.openRandomPage()
+            await base.Hamburger.clickOpen()
             await base.Hamburger.assertAbout()
         })
     })
     describe(`Logout button`, async () => {
         it(`should assert "Logout" button`, async () => {
+            await base.openRandomPage()
+            await base.Hamburger.clickOpen()
             await base.Hamburger.assertLogout()
         })
     })
     describe(`Reset App State button`, async () => {
         it(`should assert "Reset App State" button`, async () => {
+            await base.Hamburger.clickOpen()
             await base.Hamburger.assertResetAppState()
         })
     })
